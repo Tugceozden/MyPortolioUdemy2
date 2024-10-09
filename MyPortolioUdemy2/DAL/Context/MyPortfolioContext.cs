3283026dcda7cd33031d6dfe80d59d3944c8d5ce
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-10-09 18:38:11 +0300

    added  entities project

---
+++ MyPortolioUdemy2/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolioUdemy1.DAL1.Entities;
+using MyPortfolioUdemy1.Views.Entities;
+
+namespace MyPortolioUdemy2.DAL.Context
+{
+    public class MyPortfolioContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=CASPER;initial Catalog=MyPortfolioDb;integrated Security=true;");
+
+
+
+        }
+
+        public DbSet<About> Abouts{ get; set; }
+        public DbSet<Contact> Contacts{ get; set; }
+        public DbSet<Experience> Experiences{ get; set; }
+        public DbSet<Feature> Features{ get; set; }
+        public DbSet<Message> Messages{ get; set; }
+        public DbSet<Portfolio> Portfolios{ get; set; }
+        public DbSet<Skill> Skills{ get; set; }
+        public DbSet<SocialMedia> SocialMedias{ get; set; }
+        public DbSet<Testimonial> Testimonials{ get; set; }
+
+
+
+    }
+}
--- MyPortolioUdemy2/DAL/Entities/Feature.cs
+++ MyPortolioUdemy2/DAL/Entities/Feature.cs
@@ -2,5 +2,13 @@
 {
     public class Feature
     {
+        public int FeatureId { get; set; }
+        public string  Description { get; set; }
+        public string Title { get; set; }  
+        
+
+
+
+
     }
 }
