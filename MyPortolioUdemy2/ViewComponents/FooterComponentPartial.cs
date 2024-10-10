15814097c71ab87b906f868bbe1137ecd99de1c1
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-10-10 23:26:49 +0300

    added ViewComponent&Partial

---
+++ MyPortolioUdemy2/ViewComponents/FooterComponentPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortolioUdemy2.ViewComponents
+{
+    public class FooterComponentPartial:ViewComponent
+    {
+        
+        public IViewComponentResult Invoke() 
+        { 
+            return View(); 
+        }    
+        
+        
+                    
+        
+    }
+}
