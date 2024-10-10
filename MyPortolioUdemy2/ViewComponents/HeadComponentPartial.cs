f57c9a9ebb295bbcc036a6c69aebe4725d6ba900
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-10-10 21:36:10 +0300

    added  ViewComponent&View project

---
+++ MyPortolioUdemy2/ViewComponents/HeadComponentPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortolioUdemy2.ViewComponents
+{
+    public class HeadComponentPartial: ViewComponent
+    {
+
+        public IViewComponentResult Invoke() 
+        { 
+            return View();
+        } 
+
+
+
+    }
+}
---
+++ MyPortolioUdemy2/ViewComponents/NavbarComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortolioUdemy2.ViewComponents
+{
+    public class NavbarComponentPartial: ViewComponent
+    {
+        public IViewComponentResult   Invoke() 
+        { 
+            
+            return View(); 
+        }   
+
+
+    }
+}
