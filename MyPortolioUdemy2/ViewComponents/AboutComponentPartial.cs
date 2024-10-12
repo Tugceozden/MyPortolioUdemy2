65a2e0a3189f1818ca33566c7d35221863909cdf
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-10-12 21:19:15 +0300

    Added SQL&Default project

--- MyPortolioUdemy2/ViewComponents/AboutComponentPartial.cs
+++ MyPortolioUdemy2/ViewComponents/AboutComponentPartial.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolioUdemy2.DAL.Context;
 
 namespace MyPortolioUdemy2.ViewComponents
 {
     public class AboutComponentPartial:ViewComponent
     {
+        MyPortfolioContext portfolioContext=new MyPortfolioContext();
 public IViewComponentResult Invoke()
         {
+              ViewBag.aboutTitle=portfolioContext.Abouts.Select(x => x.Title).FirstOrDefault(); 
+              ViewBag.aboutSubDescription=portfolioContext.Abouts.Select(x => x.SubDescription).FirstOrDefault(); 
+              ViewBag.aboutDetail=portfolioContext.Abouts.Select(x => x.Details).FirstOrDefault(); 
             return View();  
         }
 
