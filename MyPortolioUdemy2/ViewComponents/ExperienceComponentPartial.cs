21e467ed7ca69ea591f75e9f0f2be4cf25a18d11
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-10-12 21:19:15 +0300

    Added  Controllers project

--- MyPortolioUdemy2/ViewComponents/ExperienceComponentPartial.cs
+++ MyPortolioUdemy2/ViewComponents/ExperienceComponentPartial.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolioUdemy2.DAL.Context;
 
 namespace MyPortolioUdemy2.ViewComponents
 {
     public class ExperienceComponentPartial:ViewComponent
     {
+
+        MyPortfolioContext context=new MyPortfolioContext();
 public IViewComponentResult Invoke()
         {
-            return View();  
+            var values=context.Experiences.ToList();    
+            return View(values);  
         }
 
 
--- MyPortolioUdemy2/ViewComponents/SkillComponentPartial.cs
+++ MyPortolioUdemy2/ViewComponents/SkillComponentPartial.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolioUdemy2.DAL.Context;
 
 namespace MyPortolioUdemy2.ViewComponents
 {
     public class SkillComponentPartial:ViewComponent
     {
-
+        MyPortfolioContext context=new MyPortfolioContext();
 public IViewComponentResult Invoke()
         {
-            return View();  
+            var values=context.Skills.ToList();   
+            return View(values);  
         }
 
 
