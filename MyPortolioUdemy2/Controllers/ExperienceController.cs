fa481fd5d3632a7e69b8fb4bd5325d49dc733b2f
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-10-13 20:57:20 +0300

    added Admin Panel project

---
+++ MyPortolioUdemy2/Controllers/ExperienceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortolioUdemy2.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        public IActionResult ExperienceList()
+        {
+            return View();
+        }
+    }
+}
