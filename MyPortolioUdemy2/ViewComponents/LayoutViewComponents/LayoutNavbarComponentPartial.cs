7779a70f82855c43632fb3ae1a4e4cd533d194f9
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-10-14 16:56:16 +0300

    Added Admin Panel Project

---
+++ MyPortolioUdemy2/ViewComponents/LayoutViewComponents/LayoutNavbarComponentPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortolioUdemy2.ViewComponents.LayoutViewComponents
+{
+	public class LayoutNavbarComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();	
+		}
+
+
+
+
+	}
+}
---
+++ MyPortolioUdemy2/ViewComponents/LayoutViewComponents/LayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortolioUdemy2.ViewComponents.LayoutViewComponents
+{
+	public class LayoutSidebarComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		} 
+	}
+}
