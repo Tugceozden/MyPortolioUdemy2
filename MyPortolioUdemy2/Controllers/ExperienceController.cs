7bda819c2dd1cb208b2b2d9f0eee7c2b42bd0522
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-10-15 19:23:30 +0300

    Experience section added to the project

--- MyPortolioUdemy2/Controllers/ExperienceController.cs
+++ MyPortolioUdemy2/Controllers/ExperienceController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy2.DAL.Entities;
 using MyPortolioUdemy2.DAL.Context;
 
 namespace MyPortolioUdemy2.Controllers
@@ -10,6 +11,23 @@ namespace MyPortolioUdemy2.Controllers
         {
             var values=context.Experiences.ToList();    
             return View(values);
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateExperience() 
+        {
+        
+        return View();
+        
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience) 
+        {
+            context.Experiences.Add(experience); 
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
         }
     }
 }
