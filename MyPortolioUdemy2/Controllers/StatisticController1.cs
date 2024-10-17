085c7f49395fc993513f61fb7f1461da21a81843
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-10-17 19:13:38 +0300

    Added İstatistic Project

---
+++ MyPortolioUdemy2/Controllers/StatisticController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortolioUdemy2.Controllers
+{
+	public class StatisticController1 : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
