ae3158ac358f500c3ea3eff8eaff4e629301c074
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-10-17 19:13:38 +0300

    Added İstatistic Project

---
+++ MyPortolioUdemy2/Controllers/MessageController1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolioUdemy2.DAL.Context;
+
+namespace MyPortolioUdemy2.Controllers
+{
+	public class MessageController1 : Controller
+	{
+		MyPortfolioContext context=new MyPortfolioContext();	
+		public IActionResult Inbox()
+		{
+			var values=context.Messages.ToList();	
+			return View(values);
+		}
+	}
+}
