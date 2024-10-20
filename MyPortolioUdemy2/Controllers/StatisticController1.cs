7c5c795b0ba80075392ae73ab381945c68993343
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-10-20 21:52:00 +0300

    git commit -m "Çatışmalar çözüldü

--- MyPortolioUdemy2/Controllers/StatisticController1.cs
+++ MyPortolioUdemy2/Controllers/StatisticController1.cs
@@ -1,19 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MyPortolioUdemy2.DAL.Context;
+using MyPortolioUdemy2.DAL.Context;  // Bu satır uzaktaki değişikliklerde eksikti, eklenmeli
 
 namespace MyPortolioUdemy2.Controllers
 {
 	public class StatisticController1 : Controller
 	{
-		MyPortfolioContext context=new MyPortfolioContext();
+		MyPortfolioContext context = new MyPortfolioContext();  // Yerel değişikliklerden bu satır korunmalı
 		public IActionResult Index()
 		{
 			ViewBag.v1 = context.Skills.Count();
 			ViewBag.v2 = context.Messages.Count();
-			ViewBag.v3 = context.Messages.Where(x=>x.IsRead==false).Count();
-			ViewBag.v4 = context.Messages.Where(x=>x.IsRead==true).Count();
-
-			return View();
+			ViewBag.v3 = context.Messages.Where(x => x.IsRead == false).Count();
+			ViewBag.v4 = context.Messages.Where(x => x.IsRead == true).Count();
+			return View();  // Bu satır her iki değişiklikte de aynı, korunmalı 
 		}
 	}
 }
