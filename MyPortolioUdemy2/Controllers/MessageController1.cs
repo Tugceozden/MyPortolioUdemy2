6dfb3fdc2d247fe72226c014b8825c06a9bec73a
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-11-03 21:09:40 +0300

    added message box status information project.

--- MyPortolioUdemy2/Controllers/MessageController1.cs
+++ MyPortolioUdemy2/Controllers/MessageController1.cs
@@ -11,5 +11,45 @@ namespace MyPortolioUdemy2.Controllers
 			var values=context.Messages.ToList();	
 			return View(values);
 		}
+
+		public IActionResult ChangeIsReadToTrue(int id)
+		{
+			var value = context.Messages.Find(id);
+			value.IsRead = true;	
+			context.SaveChanges();	
+			return RedirectToAction("Inbox");	
+
+		}
+		public IActionResult ChangeIsReadToFalse(int id)
+		{
+			var value = context.Messages.Find(id);
+			value.IsRead = false;
+			context.SaveChanges();
+			return RedirectToAction("Inbox");
+
+		}
+		public IActionResult DeleteMessage(int id)
+		{
+		var value=context.Messages.Find(id);	
+			context.Messages.Remove(value);
+			context.SaveChanges();
+			return RedirectToAction();
+		
+		
+		}
+		public IActionResult MessageDetail(int id)
+		{
+			var value = context.Messages.Find(id);
+				return View(value);
+
+
+
+		}
+
+
+
+
+
+
 	}
 }
